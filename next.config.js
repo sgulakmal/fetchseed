68ada3bceb569ac6567fbaa7de2c56fd9a8102e1
Udayanga Lakmal <ulakmal@fetchseed.com>
2023-07-10 21:19:41 +0530

    test

--- next.config.js
+++ next.config.js
@@ -3,4 +3,9 @@ const nextConfig = {
   reactStrictMode: true,
 }
 
-module.exports = nextConfig
+const path = require('path')
+module.exports = {
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
+  },
+}
